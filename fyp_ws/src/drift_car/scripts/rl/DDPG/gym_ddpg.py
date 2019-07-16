7bd8797973a401717c0524e3bde73169606fa552
Avan Zeng <azeng97@gmail.com>
2019-07-17 01:21:13 +1000

    add DDPG code

---
+++ fyp_ws/src/drift_car/scripts/rl/DDPG/gym_ddpg.py
@@ -0,0 +1,45 @@
+import filter_env
+from ddpg import *
+import gc
+gc.enable()
+import gym
+import gym_drift_car
+ENV_NAME = "DriftCarGazeboContinuous4WD-v0"
+EPISODES = 100000
+TEST = 10
+MAX_STEPS = 300
+
+def main():
+    env = filter_env.makeFilteredEnv(gym.make(ENV_NAME))
+    agent = DDPG(env)
+    # env.monitor.start('experiments/' + ENV_NAME,force=True)
+
+    for episode in xrange(EPISODES):
+        state = env.reset()
+        #print "episode:",episode
+        # Train
+        for step in xrange(MAX_STEPS):
+            action = agent.noise_action(state)
+            next_state,reward,done,_ = env.step(action)
+            agent.perceive(state,action,reward,next_state,done)
+            state = next_state
+            if done:
+                break
+        # Testing:
+        if episode % 100 == 0 and episode > 100:
+            total_reward = 0
+            for i in xrange(TEST):
+                state = env.reset()
+                for j in xrange(env.spec.timestep_limit):
+                    #env.render()
+                    action = agent.action(state) # direct action for test
+                    state,reward,done,_ = env.step(action)
+                    total_reward += reward
+                    if done:
+                        break
+            ave_reward = total_reward/TEST
+            print 'episode: ',episode,'Evaluation Average Reward:',ave_reward
+    env.monitor.close()
+
+if __name__ == '__main__':
+    main()
