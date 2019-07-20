43af4a729f73602bdbff9cfadea4b9c7d74a3415
Avan <azeng97@gmail.com>
2019-07-21 01:02:30 +1000

    change setup

--- setup_env.sh
+++ setup_env.sh
@@ -2,7 +2,7 @@
 
 #Install ROS
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
-sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
+sudo -E apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
 sudo apt-get update
 sudo apt-get -y install ros-kinetic-desktop-full
 sudo rosdep init
@@ -30,17 +30,17 @@ sudo apt-get -y install python-pip
 
 
 # install cuDNN v6.0
-CUDNN_TAR_FILE="cudnn-8.0-linux-x64-v6.0.tgz"
-wget http://developer.download.nvidia.com/compute/redist/cudnn/v6.0/${CUDNN_TAR_FILE}
-tar -xzvf ${CUDNN_TAR_FILE}
-sudo cp -P cuda/include/cudnn.h /usr/local/cuda-8.0/include
-sudo cp -P cuda/likdb64/libcudnn* /usr/local/cuda-8.0/lib64/
-sudo chmod a+r /usr/local/cuda-8.0/lib64/libcudnn*
+# CUDNN_TAR_FILE="cudnn-8.0-linux-x64-v6.0.tgz"
+# wget http://developer.download.nvidia.com/compute/redist/cudnn/v6.0/${CUDNN_TAR_FILE}
+# tar -xzvf ${CUDNN_TAR_FILE}
+# sudo cp -P cuda/include/cudnn.h /usr/local/cuda-8.0/include
+# sudo cp -P cuda/likdb64/libcudnn* /usr/local/cuda-8.0/lib64/
+# sudo chmod a+r /usr/local/cuda-8.0/lib64/libcudnn*
 
 # set environment variables
-echo 'export CUDA_HOME=/usr/local/cuda-8.0' >> ~/.bashrc
-echo 'export PATH=$CUDA_HOME/bin:${PATH}' >> ~/.bashrc
-echo 'export LD_LIBRARY_PATH=/usr/local/cuda-8.0/lib64:${LD_LIBRARY_PATH}' >> ~/.bashrc
+# echo 'export CUDA_HOME=/usr/local/cuda-8.0' >> ~/.bashrc
+# echo 'export PATH=$CUDA_HOME/bin:${PATH}' >> ~/.bashrc
+# echo 'export LD_LIBRARY_PATH=/usr/local/cuda-8.0/lib64:${LD_LIBRARY_PATH}' >> ~/.bashrc
 echo 'alias kd="killall -9 python || killall -9 roslaunch || killall -9 gzserver || killall -9 gzclient"' >> ~/.bashrc
 
 source ~/.bashrc
